99d4e4d3da0ece98d01c182c7c36d616f97d5446
Martin Fernandez <Martin.Fernandez@endava.net>
2020-11-11 17:16:32 -0300

    Added gastos

--- index.py
+++ index.py
@@ -5,12 +5,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('gastos.html')
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/gastos')
+def gastos():
+    return render_template('gastos.html')
+
+@app.route('/gastos_listado')
+def gastos_listado():
+    return render_template('gastos_listado.html')
 
 
 if __name__ == '__main__':
